8c73eb61a9a50f06cb4288f13ec9f1231412670c
Sean Lawrence <sean.w.lawrence@gmail.com>
2019-09-13 22:12:18 -0700

    Add syntax highlighting

---
+++ theme.js
@@ -0,0 +1,12 @@
+export default {
+  fonts: {
+    body: 'Roboto, sans-serif',
+    monospace: '"Roboto Mono", monospace',
+  },
+  colors: {
+    text: 'white',
+    background: '#e535ab',
+    primary: 'blue',
+    code: 'white',
+  },
+};
